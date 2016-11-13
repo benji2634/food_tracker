c4ba50872e6d32c7d749b62c914d4aebaa4be9c2
Tom Benham <benji26@hotmail.co.uk>
2016-11-13 21:21:59 +0000

    ran tests succesfully for Day class

--- app/src/main/java/com/example/user/foodtracker/Day.java
+++ app/src/main/java/com/example/user/foodtracker/Day.java
@@ -1,8 +1,33 @@
 package com.example.user.foodtracker;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 13/11/2016.
  */
 
 public class Day {
+    private String date;
+    private ArrayList<Meal> meals;
+
+    public Day(String date) {
+        this.date = date;
+        this.meals = new ArrayList<Meal>();
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void addMeal(Meal meal) {
+        meals.add(meal);
+    }
+
+    public int mealCount() {
+        return meals.size();
+    }
 }
--- app/src/test/java/com/example/user/foodtracker/DayTest.java
+++ app/src/test/java/com/example/user/foodtracker/DayTest.java
@@ -1,8 +1,45 @@
 package com.example.user.foodtracker;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 13/11/2016.
  */
 
 public class DayTest {
+    Day day;
+    Meal entry1;
+    Meal entry2;
+    Meal entry3;
+
+    @Before
+    public void before() {
+        day = new Day("Monday 14th November");
+        entry1 = new Meal();
+        entry2 = new Meal();
+        day.addMeal(entry1);
+        day.addMeal(entry2);
+    }
+
+    @Test
+    public void canGetName() {
+        Assert.assertEquals("Monday 14th November", day.getDate());
+    }
+
+    @Test
+    public void canSetName() {
+        day.setDate("Tuesday 15th November");
+        Assert.assertEquals("Tuesday 15th November", day.getDate());
+    }
+
+    @Test
+    public void canAddMeal() {
+        day.addMeal(entry3);
+        assertEquals(3, day.mealCount());
+    }
+
 }
