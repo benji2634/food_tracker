11da8668eca9032e113e8f34086a69fcf4e19213
Tom Benham <benji26@hotmail.co.uk>
2016-11-13 20:50:07 +0000

    adapted MealTest and created a Log class

--- app/src/main/java/com/example/user/foodtracker/Log.java
+++ app/src/main/java/com/example/user/foodtracker/Log.java
@@ -1,8 +1,25 @@
 package com.example.user.foodtracker;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 13/11/2016.
  */
 
 public class Log {
+    private ArrayList<String> messages;
+
+    public Log() {
+        this.messages = new ArrayList<String>();
+    }
+
+    public void add(String string) {
+        messages.add(string);
+    }
+
+    public void print() {
+        for (String message : messages) {
+            System.out.println(message);
+        }
+    }
 }
